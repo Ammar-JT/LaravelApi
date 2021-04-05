50ac8c673cf27582c034da520681d3d5b602139f
AmmarJT <AmmarTashk@gmail.com>
2021-04-05 15:31:20 +0300

    finished CRUD + search functions, the api is ready to use.. but there is no authentication yet

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    // ------- *insert a field in db using api -------
+    //this following code is to like a permission for u to fill the db using api:
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price'
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('products', function(){
-    return "my first api ever *ت*";
-});
+
+/*
+Route::get('products', [ProductController::class, 'index']);
+// *insert a field in db using api
+Route::post('products', [ProductController::class, 'store']);
+*/
+
+Route::resource('products', ProductController::class);
+Route::get('products/search/{name}', [ProductController::class, 'search']);
+
+
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -28,3 +39,32 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //--------------------------------------------------
 // just like laravel app: 
     // php artisan make:model Product --migration
+//--------------------------------------------------
+
+
+//--------------------------------------------------------------
+//                  *insert a field in db using api
+//--------------------------------------------------------------
+// do a routes like the above one,
+//
+// Also you have to do code in the controller, go and see it
+// ..but you must specify that in the model as fillable field,
+// ..which is a permission from laravel to allow you to fill a field in db
+// 
+// And most importantly, you have to put headers in the postman (the app that deal with apis):
+//      key = Accept, value = application/json
+// .. then you can send the body to add a new product, see it in postman
+// .. you can add the body using raw json or x-www-form-urlencoded, i used the second one.
+//
+//
+//---------------------------------------------------------------
+
+
+//--------------------------------------------------
+//                  api controllers
+//--------------------------------------------------
+// just like laravel app, but this one for api's,
+// .. notice that --api is just like --resource which make all the CRUD functions
+// .. but obviously this one is for api's:
+    // php artisan make:controller ProductController --api
+//--------------------------------------------------
